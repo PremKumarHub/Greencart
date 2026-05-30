7e8309f00cbb56c387c2a0a55ad5b504db80cd22
PremKumarHub <ey78627@gmail.com>
2026-05-30 10:23:25 +0530

    ui fix on seller's addproduct page

--- client/src/Pages/seller/AddProduct.jsx
+++ client/src/Pages/seller/AddProduct.jsx
@@ -159,7 +159,7 @@ const AddProduct = () => {
                     </div>
                 </div>
 
-                <button className="px-8 py-2.5 bg-primary text-white font-medium rounded">
+                <button className="px-8 py-2.5 bg-primary hover:bg-primary-dull transition-al text-white font-medium rounded-md cursor-pointer">
                     ADD
                 </button>
             </form>
