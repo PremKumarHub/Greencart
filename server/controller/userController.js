fc617d926d8324603d20a6ce37a1ff8514bb904a
PremKumarHub <ey78627@gmail.com>
2026-05-30 10:56:33 +0530

    adding "user creted successfully pop up message after clicking register"

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -27,7 +27,7 @@ export const register = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000,
             path: '/',
         })
-        return res.json({ success: true, user: { _id: user._id, email: user.email, name: user.name } })
+        return res.json({ success: true, message: 'User registered successfully', user: { _id: user._id, email: user.email, name: user.name } })
     }
     catch (error) {
         console.log(error.message);
