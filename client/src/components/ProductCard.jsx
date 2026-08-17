3c983f725c01499f7ffa41e5ff6d68c11ac10880
PremKumarHub <ey78627@gmail.com>
2026-08-17 14:41:38 +0530

    reverting the product category changes

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({ product }) => {
     return product && (
         <div
             onClick={() => {
-                navigate(`/products/${products.category.toLowerCase()}/${product._id}`);
+                navigate(`/products/${product.category.toLowerCase()}/${product._id}`);
                 scrollTo(0, 0)
             }}
             className="border border-gray-500/20 rounded-md md:px-4 px-2 py-2 bg-white w-full hover:shadow-sm transition-all"
